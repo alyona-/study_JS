af3ac37e5f086e66941cbfb1bbe3d5d4e0fd74ca
Alyona Omelkina <alyona_om@mail.ru>
2019-08-26 18:06:48 +0300

    Правки в задание 3.2

--- script2.js
+++ script2.js
@@ -1,13 +1,39 @@
-let num = 266219,
-    arr = num.toString().split(''),
-    mult = arr[0];
+let lang = confirm("Ru ?"),
+    ruDays = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+    enDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-for (let i = 1; i < arr.length; i++) {
-    mult *= Number(arr[i]);
+if (lang) lang = "ru";
+else lang = "en";
+
+// a. через if,
+if (lang === 'ru') {
+    console.log(ruDays);
+} else if (lang === 'en') {
+    console.log(enDays);
 }
 
-console.log(mult);
+//b. через switch-case
+switch (lang) {
+    case "ru":
+        console.log(ruDays);
+        break;
+    case "en":
+        console.log(enDays);
+        break;
+}
 
-mult = mult ** 3;
+//c. через многомерный массив без ифов и switch.
+let allDays = {
+    "ru": ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+    "en": ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+};
+console.log(allDays[lang]);
 
-console.log(mult.toString().substring(0, 2));
+//У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”,
+// если значение “Максим” то вывести в консоль “преподаватель”
+// , с любым другим значением вывести в консоль “студент”
+let namePerson = prompt("Как вас зовут?").trim();
+namePerson = namePerson[0].toUpperCase () + namePerson.substr (1).toLowerCase ();
+(namePerson === "Артем") ? console.log("директор")
+    : (namePerson === "Максим") ? console.log("преподаватель")
+    : console.log("студент");
