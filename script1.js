74f2f26a89f1f691d58ca97eb099e2a3a128a795
Alyona Omelkina <alyona_om@mail.ru>
2019-08-26 17:20:34 +0300

    3 задание часть 1

--- script1.js
+++ script1.js
@@ -1,33 +1,53 @@
 let money = 2000000,
-    income = "500000",
-    addExpenses = "коммуналка, такси, интернет, еда, вода",
-    deposit = true,
-    mission = 100000000000000,
-    period = 1;
+    income ="500000",
+    addExpenses,
+    deposit,
+    mission = 200000,
+    period =1;
 
-//Вывести в консоль тип данных значений переменных money, income, deposit;
+//1) месячный доход
+ money = +prompt("Ваш месячный доход?");
+
+//2) Возможные расходы
+addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую","интернет, еда, вода");
+addExpenses = addExpenses.replace(" ", "").split(", ");
+console.log("addExpenses: ", addExpenses);
+
+//3) Депозит
+  deposit =confirm("Есть ли у вас депозит в банке?");
+
+//4) Вывести в консоль типы данных money, income, deposit
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 
-//Вывести в консоль длину строки income
-console.log(income.length);
+//5)
+let question1 =prompt("Какие обязательные ежемесячные расходы у вас есть?"),
+    question2 =+prompt("Во сколько это обойдется?"),
+    question3 =prompt("Какие обязательные ежемесячные расходы у вас есть?"),
+    question4 =+prompt("Во сколько это обойдется?");
 
-//Вывести в консоль “Период (period) месяцев” и “Цель заработать (mission) рублей/долларов/гривен/юани”
-console.log("Период " + period + " месяцев");
-console.log("Цель заработать " + mission + " рублей/долларов/гривен/юани");
+//6) доход за месяц, учитывая обязательные расходы
+isNaN(question2) ? question2 = 0 : question2;
+isNaN(question4) ? question4 = 0 : question4;
+let budgetMonth = money - (question2 + question4);
+console.log('Доход за месяц: ', budgetMonth);
 
-//Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
-console.log(addExpenses.toLowerCase().split(", "));
+//7) за сколько месяцев будет достигнута цель mission,  округляя в большую сторону
+let countMonth = Math.ceil(mission/budgetMonth, 0);
+console.log('Цель будет достигнута за: ', countMonth , ' месяцев');
 
-//Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30), вывести в консоль результат и остаток        от деления
-let budgetDay = money / 30;
+//8) budgetDay учитывая бюджет на месяц
+let budgetDay =Math.floor(budgetMonth/30, 0);
+console.log('Ежедневный доход: ', budgetDay);
 
-//Результат money/30 хранится в переменной budgetDay
-console.log(budgetDay);
-
-//остаток от деления
-console.log(money % 30);
-
-
-
+//9)
+if(budgetDay >= 800) {
+    console.log("Высокий уровень дохода");
+}else if(budgetDay >= 300 && budgetDay < 800) {
+    console.log("Средний уровень дохода");
+}else if(budgetDay >= 0 && budgetDay < 300) {
+    console.log("Низкий уровень дохода");
+}else if(budgetDay < 0 ) {
+    console.log("Что то пошло не так");
+}
