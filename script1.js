e88a10e60821cc7dce4b00a0378ff53b9191d2c4
Alyona Omelkina <alyona_om@mail.ru>
2019-08-27 17:01:07 +0300

    Правки 4 задание

--- script1.js
+++ script1.js
@@ -1,53 +1,70 @@
-let money = 2000000,
+let money,
     income = "500000",
     addExpenses,
     deposit,
     mission = 200000,
     period = 1;
 
-//1) месячный доход
+// месячный доход
 money = +prompt("Ваш месячный доход?");
 
-//2) Возможные расходы
+// Возможные расходы за расчитываемый период записанные через запятую
 addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую", "интернет, еда, вода")
     .replace(" ", "").split(",");
 console.log("addExpenses: ", addExpenses);
 
-//3) Депозит
+// 1. a. Функция возвращает сумму всех расходов за месяц
+function getExpensesMonth(expenses1, expenses2) {
+    isNaN(expenses1) ? expenses1 = 0 : expenses1;
+    isNaN(expenses2) ? expenses2 = 0 : expenses2;
+    return expenses1 + expenses2;
+}
+
+// Депозит
 deposit = confirm("Есть ли у вас депозит в банке?");
+let showTypeof = function (item) {
+    console.log('Type: ' + item, typeof item);
+};
 
-//4) Вывести в консоль типы данных money, income, deposit
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+//Вывести в консоль типы данных money, income, deposit
+showTypeof(money);
+showTypeof(income);
+showTypeof(deposit);
 
-//5)
 let question1 = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
     question2 = +prompt("Во сколько это обойдется?"),
     question3 = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
     question4 = +prompt("Во сколько это обойдется?");
 
-//6) доход за месяц, учитывая обязательные расходы
-isNaN(question2) ? question2 = 0 : question2;
-isNaN(question4) ? question4 = 0 : question4;
-let budgetMonth = money - (question2 + question4);
-console.log('Доход за месяц: ', budgetMonth);
-
-//7) за сколько месяцев будет достигнута цель mission,  округляя в большую сторону
-let countMonth = Math.ceil(mission / budgetMonth, 0);
-console.log('Цель будет достигнута за: ', countMonth, ' месяцев');
-
-//8) budgetDay учитывая бюджет на месяц
-let budgetDay = Math.floor(budgetMonth / 30, 0);
-console.log('Ежедневный доход: ', budgetDay);
-
-//9)
-if (budgetDay >= 800) {
-    console.log("Высокий уровень дохода");
-} else if (budgetDay >= 300 && budgetDay < 800) {
-    console.log("Средний уровень дохода");
-} else if (budgetDay >= 0 && budgetDay < 300) {
-    console.log("Низкий уровень дохода");
-} else if (budgetDay < 0) {
-    console.log("Что то пошло не так");
+//1. b. Функция getAccumulateMonth Функция возвращает накопления за месяц(доходы минус расходы)
+function getAccumulatedMonth(income, expenses) {
+    return income - expenses;
 }
+
+let accumulatedMonth = getAccumulatedMonth(money, getExpensesMonth(question2, question4));
+
+//1. c. Функция getTargetMonth подсчитывает за какой период будет достигнута цель, зная результат месячного накопления
+function getTargetMonth(mission, budgetMonth) {
+    return Math.floor(mission / budgetMonth, 0);
+}
+
+console.log("Накопления за период: ", accumulatedMonth);
+
+let countMonth = getTargetMonth(mission, accumulatedMonth);
+console.log("Срок достижения цели в месяцах: ", countMonth);
+
+let budgetDay = Math.floor(budgetMonth / 30, 0);
+
+function getStatusIncome() {
+    if (budgetDay >= 800) {
+        console.log("Высокий уровень дохода");
+    } else if (budgetDay >= 300 && budgetDay < 800) {
+        console.log("Средний уровень дохода");
+    } else if (budgetDay >= 0 && budgetDay < 300) {
+        console.log("Низкий уровень дохода");
+    } else if (budgetDay < 0) {
+        console.log("Что то пошло не так");
+    }
+}
+
+console.log("getStatusIncome(): ", getStatusIncome());
