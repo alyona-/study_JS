ff63b1d92fc18d94c7be6e247e5a3cca4b69c5cd
Alyona Omelkina <alyona_om@mail.ru>
2019-09-16 10:35:13 +0300

    Изменение 7 задания

--- budget_app/script/script.js
+++ budget_app/script/script.js
@@ -1,11 +1,18 @@
 let start = document.getElementById("start"),
     btn_plus = document.getElementsByTagName("button"),
-    income_add, expenses_add,
+    income_add = document.querySelector('.income_add'),
+    expenses_add= document.querySelector('.expenses_add'),
     depositCheck =document.querySelector('#deposit-check'),
     result =document.querySelector('.result'),
     inputBudgetValue = result.querySelectorAll('div input'),
     additional_incomeItem = document.querySelectorAll('.additional_income-item'),
     budgetMonth = document.querySelector('.budget_month-value'),
+    budgetDay = document.querySelector('.budget_day-value'),
+    expensesMonth = document.querySelector('.expenses_month-value'),
+    additionalIncome = document.querySelector('.additional_income-value'),
+    additionalExpenses = document.querySelector('.additional_expenses-value'),
+    incomePeriod = document.querySelector('.income_period-value'),
+    targetMonth = document.querySelector('.target_month-value'),
     target = document.querySelector('.target-amount'),
     possibleExpense =document.querySelector('.additional_expenses-item'),
     expensesTitle = document.querySelector('.expenses-title'),
@@ -15,46 +22,3 @@ let start = document.getElementById("start"),
     incomeAmount= document.querySelector('.income-amount'),
     range = document.querySelector('.period-select');
 
-for(let i=0; i<btn_plus.length;i++){
-    let classList = btn_plus[i].classList.toString();
-    classList=classList.split(' ');
-    for(let j in classList){
-        if(classList[j] == 'income_add') {
-            income_add =btn_plus[i];
-        } else if(classList[j] == 'expenses_add'){
-            expenses_add=btn_plus[i];
-        }
-    }
-}
-
-
-function collectToArray (collect) {
-    let arr = [];
-    for (let i = collect.length >>> 0; i--;) {
-        arr[i] = collect[i];
-    }
-    return arr;
-
-}
-inputBudgetValue = collectToArray(inputBudgetValue).filter(item =>item.classList.toString().split(" ")
-    .filter(item =>item.indexOf('-value')!==-1)
-);
-
-let index1, index2,
-    class1='budget_day-value', class2='target_month-value';
-
-elem =collectToArray(inputBudgetValue);
-for(let i=0; i<elem.length; i++) {
-    let classList = elem[i].classList.toString();
-    classList=classList.split(' ');
-
-    for (let j in classList) {
-        if (classList[j] == class1) {
-            index1 = i;
-        } else if (classList[j] == class2) {
-            index2 = i;
-        }
-    }
-
-}
-inputBudgetValue = elem.slice(index1, index2+1);
