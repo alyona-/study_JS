51a688d7a4fe396618ae8a0f8f640c7dfc1aa23a
Alyona Omelkina <alyona_om@mail.ru>
2019-09-24 20:14:26 +0300

    Сохранение

--- 3dGlo/script/script.js
+++ 3dGlo/script/script.js
@@ -287,3 +287,46 @@ const slider = () => {
 };
 slider();
 
+const imagesStart = () => {
+    const cm = document.querySelector('.command');
+
+    const imgMouseOver = (target) => {
+        target.dataset.src = target.src;
+        target.src = target.dataset.img;
+    };
+
+    const imgMouseOut = (target) => {
+        target.src = target.dataset.src;
+        target.removeAttribute("data-src");
+    };
+
+    cm.addEventListener('mouseover', (event) => {
+        let target = event.target;
+        if (target.matches('.command__photo')) {
+            imgMouseOver(target);
+
+        } else return false;
+    });
+
+    cm.addEventListener('mouseout', (event) => {
+        let target = event.target;
+        if (target.matches('.command__photo')) {
+            imgMouseOut(target);
+        } else return false;
+    });
+
+};
+imagesStart();
+
+const moduleCalc = () => {
+    const calcBlock = document.querySelector('.calc-block');
+    calcBlock.addEventListener('input', (event) => {
+        let target = event.target;
+        if (target.matches('input.calc-item')) {
+            target.value = target.value.replace(/\D/gi, '');
+        }
+        return false;
+    });
+
+};
+moduleCalc();
