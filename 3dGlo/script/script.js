85a22a2ab4ca6d087a2ad0a0ac2eae714c52d04a
Alyona Omelkina <alyona_om@mail.ru>
2019-09-24 22:04:41 +0300

    Внесение изменений

--- 3dGlo/script/script.js
+++ 3dGlo/script/script.js
@@ -289,17 +289,14 @@ slider();
 
 const imagesStart = () => {
     const cm = document.querySelector('.command');
-
     const imgMouseOver = (target) => {
         target.dataset.src = target.src;
         target.src = target.dataset.img;
     };
-
     const imgMouseOut = (target) => {
         target.src = target.dataset.src;
         target.removeAttribute("data-src");
     };
-
     cm.addEventListener('mouseover', (event) => {
         let target = event.target;
         if (target.matches('.command__photo')) {
@@ -318,8 +315,17 @@ const imagesStart = () => {
 };
 imagesStart();
 
-const moduleCalc = () => {
-    const calcBlock = document.querySelector('.calc-block');
+
+//Калькулятор
+const calc = (price = 100) => {
+
+    const calcBlock = document.querySelector('.calc-block'),
+    calcType = document.querySelector('.calc-type'),
+    calcSquare = document.querySelector('.calc-square'),
+    calcDay =document.querySelector('.calc-day'),
+    calcCount = document.querySelector('.calc-count'),
+    totalValue = document.getElementById('total');
+
     calcBlock.addEventListener('input', (event) => {
         let target = event.target;
         if (target.matches('input.calc-item')) {
@@ -328,5 +334,36 @@ const moduleCalc = () => {
         return false;
     });
 
+    const countSum = () => {
+        let total = 0,
+        countValue = 1,
+        dayValue = 1;
+        const typeValue =calcType.options[calcType.selectedIndex].value,
+            squareValue = +calcSquare.value;
+
+        if(calcCount.value > 1 ) {
+            countValue += ( calcCount.value - 1 ) / 10;
+        }
+
+        if(calcDay.value && calcDay.value < 5) {
+            dayValue *= 2;
+        }else if(calcDay.value && calcDay.value < 10) {
+            dayValue *= 1.5;
+        }
+
+        if(typeValue && squareValue) {
+            total = price * typeValue * squareValue * countValue * dayValue;
+        }
+        totalValue.textContent = total;
+    };
+
+    calcBlock.addEventListener('change', (event) => {
+       const target = event.target;
+
+       if(target.matches('select') || target.matches('input')){
+            countSum();
+       }
+    });
 };
-moduleCalc();
+
+calc();
